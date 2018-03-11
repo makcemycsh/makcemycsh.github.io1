0658e8ec863f12b6f0ebe5c7c46312f1fcc5f99a
makcemycsh <m.shilo.v@yandex.ru>
2018-03-11 19:52:23 +0300

    slider-tel

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-$(document).ready(function(){
-   
-$('comments .wrap-slider').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true,
-    wrapAround: true,
-    autoPlay: true    
-});
+$(document).ready(function () {
+    $(".wrap-slider").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 8000,
+//        fade: true,
+        cssEase: 'linear',
+        appendArrows: $('.slider-arrow'),
+        prevArrow: '<div class="prev"></div>',
+        nextArrow: '<div class="next"></div>'
+    });
 });
